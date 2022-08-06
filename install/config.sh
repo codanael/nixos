d6e056e7dac78575de9837f7262de5efa74e0d0d
Codanael <43133220+codanael@users.noreply.github.com>
2022-08-06 23:15:21 +0200

    Update config.sh

--- install/config.sh
+++ install/config.sh
@@ -49,11 +49,11 @@ tee -a /mnt/etc/nixos/zfs.nix <<EOF
     enable = true;
     hostKeys = [
       {
-        path = "/persist/ssh/ssh_host_ed25519_key";
+        path = "/persist/etc/ssh/ssh_host_ed25519_key";
         type = "ed25519";
       }
       {
-        path = "/persist/ssh/ssh_host_rsa_key";
+        path = "/persist/etc/ssh/ssh_host_rsa_key";
         type = "rsa";
         bits = 4096;
       }
